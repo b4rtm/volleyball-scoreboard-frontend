c5348bea2a95bd10c92756326c986d9ca17522e8
Maksymilian <maksim2@op.pl>
2024-06-01 15:19:56 +0200

    google login system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { WebSocketProvider } from './WebSocketContext';
 import Login from './pages/Login';
 
 function App() {
   return (
+    <WebSocketProvider>
     <div className="App">
       <Login />
     </div>
+    </WebSocketProvider>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,45 @@
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { useState } from 'react';
+import { useWebSocket } from '../WebSocketContext';
+import { jwtDecode } from 'jwt-decode';
 
 const Login = () => {
 
+  const [loginError, setLoginError] = useState(null);
+  const websocket = useWebSocket();
+
   const handleGoogleLoginSuccess = (response) => {
     console.log('Logowanie Google zakończone sukcesem', response);
+    const decodedToken = jwtDecode(response.credential);
+    sendLoginRequest(decodedToken);
   };
 
+
   const handleGoogleLoginError = (response) => {
-    console.log('Błąd logowania Google', response);
+    console.log('Błąd logowania Google', response.profileObj.email);
+    setLoginError(response.error);
+  };
+
+  const sendLoginRequest = (accessToken) => {
+    const authData = {
+      login: accessToken.email,
+      password: ""
+    }
+
+    if (websocket){
+      websocket.publish({
+        destination: '/app/auth',
+        body: JSON.stringify(authData),
+      });
+
+      websocket.subscribe('/topic/authRes', message => {
+        const response = JSON.parse(message.body);
+        console.log('Odpowiedź serwera po udanym logowaniu:', response);
+      }); 
+    } else {
+        console.log("nie udało się stworzyć websocketa")
+    }
+     
   };
 
   return (
@@ -21,6 +53,7 @@ const Login = () => {
             onError={handleGoogleLoginError}
           />
         </div>
+        {loginError && <div className="text-red-500 text-center mt-2">{loginError}</div>}
       </div>
     </GoogleOAuthProvider>
   );
