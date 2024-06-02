db7ce76511c1cb9de88756bc476f0281679d0d1c
b4rtm <bartek.matusiak12@gmail.com>
2024-06-02 17:49:20 +0200

    move websocket to startPage

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AddMatch from '../components/AddMatch';
 import UserInfoNav from '../components/UserInfoNav';
+import { useWebSocket } from '../WebSocketContext';
+import ManageTeams from '../components/ManageTeams';
 
 const StartPage = () => {
+    const [teams, setTeams] = useState([]);
+    const websocket = useWebSocket();
+
+    useEffect(() => {
+        const handleTeamsMessage = (message) => {
+            const data = JSON.parse(message.body);
+            console.log('Received teams data:', data);
+            setTeams(data);
+        };
+
+        if (websocket) {
+            websocket.onConnect = () => {
+                websocket.subscribe('/topic/teams', handleTeamsMessage);
+                websocket.publish({ destination: '/app/getTeams' });
+            };
+        }
+
+        return () => {
+            if (websocket) {
+                websocket.disconnect();
+            }
+        };
+    }, [websocket]);
+
     return (
         <div>
             <UserInfoNav />
             <h1 className="text-2xl font-bold text-center mb-4">Start Page</h1>
-            <AddMatch />
+            <AddMatch teams={teams} websocket={websocket}/>
+            <ManageTeams teams={teams} websocket={websocket} />
         </div>
     );
 };
